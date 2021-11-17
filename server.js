b267b4f89e4b0310ee5b8fb07d5534ba1756061b
yyixin-2019 <46566937+yyixin-2019@users.noreply.github.com>
2021-11-17 16:30:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-	const info = stmt.run(req.body.user, req.body.pass);
+	const info = stmt.run(req.body.user, md5(req.body.pass));
 	res.json({"message": info.changes+ " record created: ID " +info.lastInsertRowid +" (201)"});
 	res.status(201);
 });
@@ -45,7 +45,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	const info = stmt.run(req.body.user, req.body.pass, req.params.id);
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
 	//res.status(200).send(info.changes+ " record updated: ID " +info.lastInsertRowid);
 	res.json({"message": info.changes+ " record updated: ID " +req.params.id +" (200)"});
 	res.status(200);
